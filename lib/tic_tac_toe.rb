a24c224ac48b1e50ff334635b5522dd72d8a50ec
Daniel Sasser <danielbsasser@gmail.com>
2020-02-21 14:37:37 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -23,9 +23,9 @@ class TicTacToe
     puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
   end
 
-def input_to_index(input)
-  input.to_i - 1
-end
+  def input_to_index(input)
+    input.to_i - 1
+  end
 
 def move(input, token="X")
   @board[input] = token
