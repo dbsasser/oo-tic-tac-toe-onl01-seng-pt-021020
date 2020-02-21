ee4725a7b29cc96952b47f7220cbd3657215a0f5
Daniel Sasser <danielbsasser@gmail.com>
2020-02-21 16:31:05 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -76,9 +76,9 @@ class TicTacToe
       # pos_2 = combo[1]
       # pos_3 = combo[2]
       
-      if @board[combo[0]] == "X" && @board[combo[1]] == "X" && @board[combo[2] == "X"
+      if @board[combo[0]] == "X" && @board[combo[1]] == "X" && @board[combo[2]] == "X"
         return combo 
-      elsif (@board[combo[0]]) == "O" && (@board[combo[1]]) == "O" && (@board[combo[2]]) == "O"
+      elsif @board[combo[0]] == "O" && @board[combo[1]] == "O" && @board[combo[2]] == "O"
         return combo 
       else
         false 
